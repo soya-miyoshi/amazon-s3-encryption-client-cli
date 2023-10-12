2ddc5994b30437f75cd47e5df8823a2b68de6ea0
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-12 14:46:39 +0900

    deleted some unused code

--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -3,10 +3,6 @@ package com.github.soyamiyoshi;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.encryption.s3.S3EncryptionClient;
 
-/**
- * Hello world!
- *
- */
 public class App {
 
     public static void main(String[] args) {
@@ -19,6 +15,5 @@ public class App {
         v3Client.putObject(null, null);
 
         v3Client.close();
-        System.out.println("Hello World!");
     }
 }

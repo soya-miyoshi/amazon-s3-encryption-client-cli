a910bfd1c8136829eaf9d99b34d65a8fc47aad30
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-12 14:39:06 +0900

    modified formatter setting

--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -9,21 +9,16 @@ import software.amazon.encryption.s3.S3EncryptionClient;
  */
 public class App {
 
-    public static void main(
-            String[] args) {
+    public static void main(String[] args) {
         S3Client v3Client =
                 S3EncryptionClient
                         .builder()
-                        .kmsKeyId(
-                                kmsKeyId)
+                        .kmsKeyId(kmsKeyId)
                         .build();
-        v3Client.putObject(
-                null,
-                null);
+
+        v3Client.putObject(null, null);
 
         v3Client.close();
-        System.out
-                .println(
-                        "Hello World!");
+        System.out.println("Hello World!");
     }
 }

e3660dfd16f04153dfa253badf371b070fb02d80
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-12 13:31:25 +0900

    formatter 追加

--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -1,6 +1,7 @@
 package com.github.soyamiyoshi;
 
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.encryption.s3.S3EncryptionClient;
 
 /**
  * Hello world!
@@ -9,6 +10,9 @@ import software.amazon.awssdk.services.s3.S3Client;
 public class App {
 
     public static void main(String[] args) {
+        S3Client v3Client = S3EncryptionClient.builder().kmsKeyId(kmsKeyId).build();
+        v3Client.putObject(null, null);
+        v3Client.close();
         System.out.println("Hello World!");
     }
 }

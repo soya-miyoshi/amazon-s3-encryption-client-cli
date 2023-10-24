29e7f1c22c9ff2dc3d48f407a6b05cfdec4e95d5
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-24 13:38:06 +0900

    WIP

--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -1,11 +1,13 @@
 package com.github.soyamiyoshi;
 
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.KeyFactory;
 import java.security.PublicKey;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
@@ -19,33 +21,65 @@ public class App {
     private static final String PUBLIC_KEY_PATH =
             System.getenv("PUBLIC_KEY_PATH");
 
-    // TODO: Handle exceptions
-    private static PublicKey loadPublicKey()
+    private static PublicKey loadPemFormatPublicKey()
             throws Exception {
-        try {
-            byte[] keyBytes = Files
-                    .readAllBytes(Paths.get(
-                            PUBLIC_KEY_PATH));
-            X509EncodedKeySpec spec =
-                    new X509EncodedKeySpec(
-                            keyBytes);
-            KeyFactory keyFactory =
-                    KeyFactory.getInstance(
-                            "RSA");
-            PublicKey publicKey = keyFactory
-                    .generatePublic(spec);
-            return publicKey;
-        } catch (Exception e) {
-            throw e;
-        }
+        String pemContent = new String(
+                Files.readAllBytes(Paths.get(
+                        PUBLIC_KEY_PATH)),
+                StandardCharsets.UTF_8);
+
+        // Remove the first and last lines (the PEM header and footer)
+        String publicKeyPEM = pemContent
+                .replace(
+                        "-----BEGIN PUBLIC KEY-----",
+                        "")
+                .replace(
+                        "-----END PUBLIC KEY-----",
+                        "")
+                .replaceAll("\\s", ""); // This will remove newlines and spaces
+
+        byte[] decodedBytes =
+                Base64.getDecoder().decode(
+                        publicKeyPEM);
+        X509EncodedKeySpec spec =
+                new X509EncodedKeySpec(
+                        decodedBytes);
+        KeyFactory keyFactory = KeyFactory
+                .getInstance("RSA");
+        return keyFactory
+                .generatePublic(spec);
     }
+
+
+    // TODO: Handle exceptions
+    // private static PublicKey loadPublicKey()
+    // throws Exception {
+    // try {
+    // byte[] keyBytes = Files
+    // .readAllBytes(Paths.get(
+    // PUBLIC_KEY_PATH));
+    // X509EncodedKeySpec spec =
+    // new X509EncodedKeySpec(
+    // keyBytes);
+    // KeyFactory keyFactory =
+    // KeyFactory.getInstance(
+    // "RSA");
+    // PublicKey publicKey = keyFactory
+    // .generatePublic(spec);
+    // return publicKey;
+    // } catch (Exception e) {
+    // throw e;
+    // }
+    // }
+
 
     private static void uploadUsingPublicKeyEncryption(
             String objectKey,
             Path filePath) {
         PublicKey publicKey = null;
         try {
-            publicKey = loadPublicKey();
+            publicKey =
+                    loadPemFormatPublicKey();
         } catch (Exception e) {
             System.out.println(
                     "failed at loadPublicKey()");

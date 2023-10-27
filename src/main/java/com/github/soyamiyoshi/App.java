80740be42d182f004db3288adba2601667295dbe
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-27 20:52:04 +0900

    make bucket name as env value

--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -5,20 +5,21 @@ import java.nio.file.Paths;
 import java.security.PublicKey;
 import java.util.Optional;
 import com.github.soyamiyoshi.uploadclient.PublicKeyAsyncUploadClient;
+import static com.github.soyamiyoshi.util.EnvLoader.getEnvOrExit;
 import static com.github.soyamiyoshi.util.KeyLoader.loadPemFormatPublicKey;
 
 public class App {
 
     public static void main(String[] args) {
-        if (args.length < 3) {
+        if (args.length < 2) {
             System.out.println(
-                    "Usage: java App <BUCKET_NAME> <OBJECT_KEY> <UPLOAD_FILE_PATH>");
+                    "Usage: java App <OBJECT_KEY> <UPLOAD_FILE_PATH>");
             return;
         }
 
-        String bucketName = args[0];
-        String objectKey = args[1];
-        Path uploadFilePath = Paths.get(args[2]);
+        String bucketName = getEnvOrExit("BUCKET_NAME");
+        String objectKey = args[0];
+        Path uploadFilePath = Paths.get(args[1]);
 
         Optional<PublicKey> publicKey = loadPemFormatPublicKey();
         if (publicKey.isEmpty()) {

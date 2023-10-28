ebbce7c4ccdd938789fc56055b29cceb76c326cf
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-28 16:12:07 +0900

    handle error when no such file

--- src/main/java/com/github/soyamiyoshi/uploadclient/PublicKeyAsyncUploadClient.java
+++ src/main/java/com/github/soyamiyoshi/uploadclient/PublicKeyAsyncUploadClient.java
@@ -1,8 +1,10 @@
 package com.github.soyamiyoshi.uploadclient;
 
+import java.io.UncheckedIOException;
 import java.nio.file.Path;
 import java.security.PublicKey;
 import java.util.concurrent.CompletableFuture;
+import com.beust.ah.A;
 import software.amazon.awssdk.core.async.AsyncRequestBody;
 import software.amazon.awssdk.services.s3.S3AsyncClient;
 import software.amazon.awssdk.services.s3.model.PutObjectResponse;
@@ -24,11 +26,20 @@ public class PublicKeyAsyncUploadClient extends UploadClient {
         S3AsyncClient v3AsyncClient = S3AsyncEncryptionClient.builder()
                 .rsaKeyPair(new PartialRsaKeyPair(null, publicKey))
                 .build();
+
+        AsyncRequestBody asyncRequestBody = null;
+
+        try {
+            asyncRequestBody = AsyncRequestBody.fromFile(localFilePath);
+        } catch (UncheckedIOException e) {
+            System.err.println("Error creating asyncRequestBody");
+            System.exit(1);
+        }
         CompletableFuture<PutObjectResponse> futurePut =
                 v3AsyncClient.putObject(builder -> builder
                         .bucket(bucketName)
                         .key(objectKey)
-                        .build(), AsyncRequestBody.fromFile(localFilePath));
+                        .build(), asyncRequestBody);
         futurePut.join();
         v3AsyncClient.close();
     }

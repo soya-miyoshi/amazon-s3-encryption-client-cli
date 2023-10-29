d1881b1ae2d9fc643a9fb2e5ce54e4ee01849a0a
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-30 00:01:03 +0900

    Download using private key

--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -1,17 +1,25 @@
 package com.github.soyamiyoshi;
 
+import static com.github.soyamiyoshi.Act.downloadAsync;
+import static com.github.soyamiyoshi.Act.uploadAsync;
 import static com.github.soyamiyoshi.util.EnvLoader.getEnvOrExit;
 import java.nio.file.Path;
-import com.github.soyamiyoshi.act.Act;
 import com.github.soyamiyoshi.cli.ArgsParser;
 import com.github.soyamiyoshi.cli.CommandLineArgs;
 
 public class App {
     public static void main(String[] args) {
         CommandLineArgs cliArgs = ArgsParser.parse(args);
+
         if (cliArgs.isUpload()) {
-            Act.uploadAsync(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey(),
+            uploadAsync(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey(),
                     Path.of(cliArgs.getLocalFilePath()));
         }
+        if (cliArgs.isDownload()) {
+            downloadAsync(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey());
+            System.exit(1);
+        }
+
+        return;
     }
 }
---
+++ src/main/java/com/github/soyamiyoshi/client/download/PrivateKeyAsyncDownloadClient.java
@@ -0,0 +1,40 @@
+package com.github.soyamiyoshi.client.download;
+
+import java.security.PrivateKey;
+import java.util.concurrent.CompletableFuture;
+import static com.github.soyamiyoshi.util.ObjectSaver.saveToFile;
+import software.amazon.awssdk.core.ResponseBytes;
+import software.amazon.awssdk.core.async.AsyncResponseTransformer;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+import software.amazon.encryption.s3.S3AsyncEncryptionClient;
+import software.amazon.encryption.s3.materials.PartialRsaKeyPair;
+
+public class PrivateKeyAsyncDownloadClient extends DownloadClient {
+
+    public PrivateKeyAsyncDownloadClient(PrivateKey privateKey) {
+        super(privateKey);
+    }
+
+    @Override
+    public void download(
+            final String bucketName,
+            final String objectKey) {
+
+        S3AsyncClient v3AsyncClient = S3AsyncEncryptionClient.builder()
+                .rsaKeyPair(new PartialRsaKeyPair(privateKey, null))
+                .build();
+
+        CompletableFuture<ResponseBytes<GetObjectResponse>> futureGet =
+                v3AsyncClient.getObject(builder -> builder
+                        .bucket(bucketName)
+                        .key(objectKey)
+                        .build(), AsyncResponseTransformer.toBytes());
+
+        ResponseBytes<GetObjectResponse> getResponse = futureGet.join();
+
+        saveToFile(getResponse.asInputStream(), objectKey);
+
+    }
+
+}

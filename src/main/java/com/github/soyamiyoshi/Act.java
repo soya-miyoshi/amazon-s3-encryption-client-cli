b4c85f6211f262c712b5811132b0b9605c902eed
Soya Miyoshi <soya.miyoshi@gmail.com>
2023-10-30 13:33:14 +0900

    rename

--- src/main/java/com/github/soyamiyoshi/Act.java
+++ src/main/java/com/github/soyamiyoshi/Act.java
@@ -6,12 +6,12 @@ import java.nio.file.Path;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.util.Optional;
-import com.github.soyamiyoshi.client.download.PrivateKeyAsyncDownloadClient;
-import com.github.soyamiyoshi.client.upload.PublicKeyAsyncUploadClient;
+import com.github.soyamiyoshi.client.download.BlockingDownloader;
+import com.github.soyamiyoshi.client.upload.BlockingUploader;
 
 public class Act {
 
-    public static void uploadAsync(final String bucketName, final String objectKey,
+    public static void blockingUpload(final String bucketName, final String objectKey,
             final Path uploadFilePath) {
         Optional<PublicKey> publicKey = loadPemFormatPublicKey();
         if (publicKey.isEmpty()) {
@@ -19,22 +19,22 @@ public class Act {
             System.exit(1);
             return;
         }
-        PublicKeyAsyncUploadClient publicKeyAsyncUploadClient =
-                new PublicKeyAsyncUploadClient(publicKey.get());
+        BlockingUploader blockUploadClient =
+                new BlockingUploader(publicKey.get());
 
-        publicKeyAsyncUploadClient.upload(bucketName, objectKey, uploadFilePath);
+        blockUploadClient.upload(bucketName, objectKey, uploadFilePath);
     }
 
-    public static void downloadAsync(final String bucketName, final String objectKey) {
+    public static void blockingDownload(final String bucketName, final String objectKey) {
         Optional<PrivateKey> privateKey = loadPemFormatPrivateKey();
         if (privateKey.isEmpty()) {
             System.err.println("Error loading private key");
             System.exit(1);
             return;
         }
-        PrivateKeyAsyncDownloadClient privateKeyAsyncDownloadClient =
-                new PrivateKeyAsyncDownloadClient(privateKey.get());
+        BlockingDownloader blockDownloadClient =
+                new BlockingDownloader(privateKey.get());
 
-        privateKeyAsyncDownloadClient.download(bucketName, objectKey);
+        blockDownloadClient.download(bucketName, objectKey);
     }
 }
--- src/main/java/com/github/soyamiyoshi/App.java
+++ src/main/java/com/github/soyamiyoshi/App.java
@@ -1,7 +1,7 @@
 package com.github.soyamiyoshi;
 
-import static com.github.soyamiyoshi.Act.downloadAsync;
-import static com.github.soyamiyoshi.Act.uploadAsync;
+import static com.github.soyamiyoshi.Act.blockingDownload;
+import static com.github.soyamiyoshi.Act.blockingUpload;
 import static com.github.soyamiyoshi.util.EnvLoader.getEnvOrExit;
 import java.nio.file.Path;
 import com.github.soyamiyoshi.cli.ArgsParser;
@@ -15,7 +15,7 @@ public class App {
             // output as a log
             System.out.println("Uploading " + cliArgs.getLocalFilePath() + " to "
                     + cliArgs.getObjectKey() + "...");
-            uploadAsync(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey(),
+            blockingUpload(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey(),
                     Path.of(cliArgs.getLocalFilePath()));
             // output as a log
             System.out.println("Upload completed.");
@@ -23,7 +23,7 @@ public class App {
         if (cliArgs.isDownload()) {
             // output as a log
             System.out.println("Downloading " + cliArgs.getObjectKey() + "...");
-            downloadAsync(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey());
+            blockingDownload(getEnvOrExit("BUCKET_NAME"), cliArgs.getObjectKey());
             System.out.println("Download completed.");
         }
 
